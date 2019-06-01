39f64cec8e9256033666a4b1cb81aaf185c405d6
Niklas Wang <niklas.wang@outlook.com>
2019-06-01 17:00:56 +0800

    06/01/2019: Initialize GUI which is implemented by Qt 5.12.3

---
+++ gui/MainWindow.cpp
@@ -0,0 +1,14 @@
+#include "MainWindow.h"
+#include "ui\MainWindowUi.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
