4a20cee2b2badb6ed15b76ddf686e0223411ade5
lihuibin <lihuibin705@163.com>
2019-06-06 10:30:09 +0800

    06/06/2019 [For monitor]: Generates file informations through directory scan

---
+++ monitor/FileManager.h
@@ -0,0 +1,51 @@
+/**
+ * @file FileManager.h
+ * @brief
+ * @author  lenovo <xxx@lenovo.com>
+ * @version 1.0.0
+ * @date 2019-06-04
+ */
+/* Copyright(C) 2009-2019, Lenovo Inc.
+ * All right reserved
+ *
+ */
+#ifndef __FILE_MANAGER_H__
+#define __FILE_MANAGER_H__
+
+#include <string>
+#include <map>
+
+#include "common.h"
+#include "inicpp.h"
+namespace uranium
+{
+
+#define FILE_MANAGER_DEFAULT_NAME   "fileManager.ini"
+#define FILE_MANAGER_SECTION        "FileManager"
+
+typedef struct FILEINFOS_TAG {
+    uint32_t    checksum[4];
+} FILEINFOS_T;
+
+class FileManager
+{
+public:
+    FileManager();
+    ~FileManager();
+    int32_t fileTarFromPath(const std::string fromPath, const std::string compreFile);
+    int32_t fileUntarToPath(const std::string compreFile, const std::string toPath);
+    int32_t fileInfosSave(const std::string path);
+    int32_t fileInfosLoad(const std::string path);
+    int32_t fileScanToInis(const std::string path);
+    int32_t filePathSet(const std::string path);
+private:
+    int32_t fileInfoErase(void);
+    ModuleType      mModule;
+    std::string     mfilePath;
+    ini::IniFile    mFile;
+    std::map<std::string, std::string> mFileInfos;
+    uint32_t        myChecksum[4];
+};
+
+};
+#endif // __FILE_MANAGER_H__
---
+++ monitor/tester/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file main.cpp
+ * @brief
+ * @author  lenovo <xxx@lenovo.com>
+ * @version 1.0.0
+ * @date 2019-06-04
+ */
+/* Copyright(C) 2009-2019, Lenovo Inc.
+ * All right reserved
+ *
+ */
+#include "FileManager.h"
+#include "md5.h"
+int main(int argc, char **argv)
+{
+    uranium::FileManager fileMang;
+
+    fileMang.fileScanToInis("/mnt/d/lenvov_wokspace/source/Uranium");
+    fileMang.fileInfosSave("/tmp/lhb.ini");
+    return 0;
+}
