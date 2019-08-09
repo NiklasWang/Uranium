2fc455afc8c72e613da89300c63cb1437160fd93
lihuibin <lihuibin705@163.com>
2019-08-09 11:22:52 +0800

    08/09/2019 Perfect server programs

--- core/tester/server_tester.cpp
+++ core/tester/server_tester.cpp
@@ -3,9 +3,20 @@
 #include "ServiceCore.h"
 
 using namespace uranium;
+void helpPrint(const char *funcName)
+{
+    printf("Usage: %s User_name User_password\n", funcName);
+    printf("%s lihb13 testPasswd\n", funcName);
+}
+
 int main(int argc, char **argv)
 {
-    ServiceCore serCore(TRANS_SERVER, " ", "lihb13", "Lhbzyy8629");
+    if (argc < 3) {
+        helpPrint(argv[0]);
+        return -1;
+    }
+
+    ServiceCore serCore(TRANS_SERVER, " ", argv[1], argv[2]);
     serCore.construct();
     serCore.initialize();
     serCore.start();
